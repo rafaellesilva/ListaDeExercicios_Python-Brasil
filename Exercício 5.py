090b69dce7a1fadfac5ce2fbcb167c9a728183fa
Rafaelle Cristine <88450265+rafaellecristine@users.noreply.github.com>
2022-01-18 16:26:42 -0300

    Delete Exercício 5.py

--- Exercício 5.py
+++
@@ -1,3 +0,0 @@
-#Faça um Programa que converta metros para centímetros.
-metros = int(input('Informe um valor em metros para ser convertido em centímetros:'))
-print(f'Em metros: {metros}m\nEm centímetros: {metros * 100}cm')

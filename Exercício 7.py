600abcb6dee486304c2e5509dec54bdba1bed8e1
Rafaelle Cristine <88450265+rafaellecristine@users.noreply.github.com>
2022-01-18 16:26:23 -0300

    Delete Exercício 7.py

--- Exercício 7.py
+++
@@ -1,4 +0,0 @@
-# Faça um Programa que calcule a área de um quadrado, em seguida mostre o dobro desta área para o usuário.
-lado = int(input('Usuário, informe o lado do quadrado para o cálculo da área: '))
-print(f'Usuário, a área do quadrado é: {(lado**2)} cm²')
-print(f'Usuário, o dobro desta área é: {(lado**2) + (lado**2)} cm²')

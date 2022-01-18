46d3e714c507c12ed308cb225bf1a893258fa810
Rafaelle Cristine <88450265+rafaellecristine@users.noreply.github.com>
2022-01-18 16:26:50 -0300

    Delete Exercício 4.py

--- Exercício 4.py
+++
@@ -1,4 +0,0 @@
-# Faça um Programa que peça as 4 notas bimestrais e mostre a média
-nota_1,nota_2,nota_3,nota_4 = map(float,input('Informe as 4 notas bimestrais:').split())
-media = (nota_1 + nota_2 + nota_3 + nota_4) / 4
-print(f'A média do aluno é: {round(media)}')

2962411c119fbde5523ec39d8cc187ee69ab1840
Rafaelle Cristine <88450265+rafaellecristine@users.noreply.github.com>
2022-01-18 16:26:09 -0300

    Delete Exercício 8.py

--- Exercício 8.py
+++
@@ -1,3 +0,0 @@
-#Faça um Programa que pergunte quanto você ganha por hora e o número de horas trabalhadas no mês. Calcule e mostre o total do seu salário no referido mês.
-valor_hora,horas_trabalhadas = map(float,input('Usuário, informe o valor que ganha por hora e o número de horas trabalhadas para o cálculo total de seu salário: ').split())
-print(f'Usuário, seu salário é de: R$ {valor_hora * horas_trabalhadas:.2f}')

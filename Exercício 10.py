01ce0fd1c0cf949625a58f5d88f1a5b95da9b130
Rafaelle Cristine <88450265+rafaellecristine@users.noreply.github.com>
2022-01-18 16:28:59 -0300

    Delete Exercício 10.py

--- Exercício 10.py
+++
@@ -1,4 +0,0 @@
-#Faça um Programa que peça a temperatura em graus Celsius, transforme e mostre em graus Fahrenheit.
-graus_Celsius = int(input('Informe a temperatura em graus Celsius: '))
-F = (graus_Celsius * (9/5)) + 32
-print(f'A temperatura em Celsius {graus_Celsius:.0f}°C convertida para graus Fahrenheit {F:.0f}°F')

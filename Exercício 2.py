727c1f5c50f11918182df6230559da93ae5e724e
Rafaelle Cristine <88450265+rafaellecristine@users.noreply.github.com>
2022-01-18 16:27:08 -0300

    Delete Exercício 2.py

--- Exercício 2.py
+++
@@ -1,8 +0,0 @@
-# Faça um Programa que peça um número e então mostre a mensagem O número informado foi [número].
-try:
-    number = int(input('Informe um número inteiro:'))
-except ValueError:
-    print('O número que você digitou não é um valor inteiro')
-    number = int(input('Informe um número inteiro:'))
-finally:
-    print(f'O número informado foi {number}')

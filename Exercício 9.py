66317ae280beef77c42270d26cd9a53ce86b1e82
Rafaelle Cristine <88450265+rafaellecristine@users.noreply.github.com>
2022-01-18 16:26:00 -0300

    Delete Exercício 9.py

--- Exercício 9.py
+++
@@ -1,4 +0,0 @@
-#Faça um Programa que peça a temperatura em graus Fahrenheit, transforme e mostre a temperatura em graus Celsius.
-graus_Fahrenheit = float(input('Informe a temperatura em graus Fahrenheit: '))
-C = 5 * ((graus_Fahrenheit-32) / 9)
-print(f'A temperatura em Fahrenheit {graus_Fahrenheit:.0f}°F convertida em graus Celsius {C:.0f}°C')
